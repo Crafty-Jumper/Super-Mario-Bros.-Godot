73dc16aa5d1938991ecb37a96558f7cf499ffe59
Crafty Jumper <blueghostbear@gmail.com>
2025-07-15 20:16:32 -0700

    Delete Most files to prepare for github desktop

--- scripts/questionblock.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-@onready var timer: Timer = $"../Timer"
-
-func _process(_delta: float) -> void:
-	var color = floor(timer.time_left * 5)
-	material.set_shader_parameter("accessRow",fmod(color,4)+1 + (GlobalVariables.theme * 4))
