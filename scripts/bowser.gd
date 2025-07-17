8f6a581f085bad8690808e35f98f3b1d0cfd8ce2
Crafty Jumper <blueghostbear@gmail.com>
2025-07-16 18:14:06 -0700

    increase reaction from fireballs to bowser

--- scripts/bowser.gd
+++ scripts/bowser.gd
@@ -28,7 +28,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "fireball":
+	if body.is_in_group("fireball"):
 		body.explode()
 		health -= 1
 
