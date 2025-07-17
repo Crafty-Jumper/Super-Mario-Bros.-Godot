e5837572a79989ad05f96c1c665898ed0e2a82c1
Crafty Jumper <blueghostbear@gmail.com>
2025-07-16 18:03:34 -0700

    fix star music names, update bowser

--- scripts/bowser.gd
+++ scripts/bowser.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var area_2d: Area2D = $Area2D
+
 const gravity = 10
+var health = 10
 
 func _physics_process(delta: float) -> void:
 	
@@ -11,6 +16,21 @@ func _physics_process(delta: float) -> void:
 		if randi_range(1,250) == 1:
 			velocity.y = -250
 	
+	if health == 0:
+		flip()
+	
 
 
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "fireball":
+		body.explode()
+		health -= 1
+
+func flip() -> void:
+	animated_sprite_2d.flip_v = true
+	area_2d.queue_free()
+	collision_shape_2d.disabled = true
+	velocity.y = -300
