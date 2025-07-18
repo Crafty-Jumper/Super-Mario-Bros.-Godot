29bbcdf12996b32f6e346daa1b97d3fed8adbdd2
Crafty Jumper <blueghostbear@gmail.com>
2025-07-17 18:07:29 -0700

    failed piranha plant 2: electric boogaloo

--- scenes/entities/piranha_plant.gd
+++ scenes/entities/piranha_plant.gd
@@ -2,18 +2,19 @@ extends Area2D
 
 var origY = 0
 var canMove : bool = true
-var moveDir = -1
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var moveDir = 1
+@onready var timer: Timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	origY = position.y
 	position.y += 24
-	animation_player.play()
+	timer.start(2)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y -= 60 * moveDir * delta
+	
+	position.y -= 60*moveDir * delta
+	
 	if position.y < origY:
 		position.y = origY
 	if position.y > origY + 24:
@@ -24,3 +25,12 @@ func moveUp() -> void:
 
 func moveDown() -> void:
 	moveDir = 1
+
+
+func _on_timer_timeout() -> void:
+	if moveDir == -1:
+		moveDir = 1
+		timer.start(2)
+	if moveDir == 1:
+		moveDir = -1
+		timer.start(4)
