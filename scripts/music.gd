8262d7af294c82348aa62270e14ba7d8b356c3b6
Crafty Jumper <blueghostbear@gmail.com>
2025-07-18 20:20:20 -0700

    successfully add global music

--- scripts/music.gd
+++ scripts/music.gd
@@ -6,8 +6,8 @@ const songs = ["Overworld","Underground","Underwater","Castle","Star"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	stream = load("res://scenes/level.tscn::AudioStreamSynchronized_2e2fo")
-
+	stream = load("res://music.tres")
+	loadtrack("Overworld")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
