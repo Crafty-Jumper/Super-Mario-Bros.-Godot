87eb45994b8554236c278101a5c7b80f4b311493
Crafty Jumper <blueghostbear@gmail.com>
2025-07-22 21:44:53 -0700

    update a whole lotta stuff

--- scripts/flagpole.gd
+++ scripts/flagpole.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var audio_stream_player_2d: AudioStreamPlayer = $AudioStreamPlayer2D
 
 var inUse : bool = false
 
