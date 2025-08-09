80245ed72847a7d3e5af819fd3bb39544833728b
Crafty Jumper <blueghostbear@gmail.com>
2025-08-09 13:46:29 -0700

    Revert "am i screwed"
    This reverts commit a8950657e34a76b4fb1dda85ca943cb8bda7e240.

--- scripts/autoloads/global_variables.gd
+++ scripts/autoloads/global_variables.gd
@@ -18,7 +18,12 @@ var theme = 0
 var song = 0
 var levelpack = "SMB"
 var levelPrefix = str(world) + "-" + str(level) + "." + str(sub)
-var levelPath = "res://Level Data/" + levelpack + "/" + levelPrefix + ".tmx"
+var leveldata = FileAccess.open("res://Level Data/" + levelpack + "/leveldata.json", FileAccess.READ)
+var leveldatajson = JSON.parse_string(leveldata.get_as_text())
+var levelPath = "res://Level Data/" + levelpack + "/" + levelPrefix
+var levelHeight = leveldatajson[levelPrefix]["levelHeight"]
+var levelWidth = leveldatajson[levelPrefix]["levelWidth"]
+var levelBGColor = leveldatajson[levelPrefix]["bgCol"]
 var marioScreen = 0
 var marioOffset = 0
 
@@ -54,11 +59,35 @@ func _process(delta: float) -> void:
 		marioInvinc -= 60 * delta
 	else:
 		marioInvinc = 0
+		
+	
+	
+		
+	
+
+func map(input_min: float, input_max: float, output_min: float, output_max: float, input: float):
+	var output = input - input_min
+	output = output/input_max
+	output = output*output_max
+	output = output+output_min
+	return output
+
 
 func fixpath() -> void:
 	GlobalVariables.levelpack = Save.config.get_value("Misc","levelPack","SMB")
+	song = leveldatajson[levelPrefix]["song"]
 	levelPrefix = str(world) + "-" + str(level) + "." + str(sub)
-	if FileAccess.file_exists("res://Level Data/" + levelpack + "/" + levelPrefix + ".tmx"):
-		levelPath = "res://Level Data/" + levelpack + "/" + levelPrefix + ".tmx"
+	if FileAccess.file_exists("res://Level Data/" + levelpack + "/" + levelPrefix + "_Tiles.csv"):
+		levelPath = "res://Level Data/" + levelpack + "/" + levelPrefix
 	else:
-		levelPath = "user://data/Level Packs/" + levelpack + "/" + levelPrefix + ".tmx"
+		levelPath = "user://data/Level Packs/" + levelpack + "/" + levelPrefix
+	if FileAccess.file_exists("res://Level Data/" + levelpack + "/leveldata.json"):
+		leveldata = FileAccess.open("res://Level Data/" + levelpack + "/leveldata.json", FileAccess.READ)
+	else:
+		leveldata = FileAccess.open("user://data/Level Packs/" + levelpack + "/leveldata.json", FileAccess.READ)
+	theme = leveldatajson[levelPrefix]["levelTheme"]
+	levelHeight = leveldatajson[levelPrefix]["levelHeight"]
+	levelWidth = leveldatajson[levelPrefix]["levelWidth"]
+	levelBGColor = leveldatajson[levelPrefix]["bgCol"]
+	marioOffset = leveldatajson[levelPrefix]["pipescreen"][marioScreen]
+	
