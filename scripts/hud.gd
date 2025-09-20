75c8ccc75b41fdd0fc2653f8bba07b258c29c75c
Crafty Jumper <blueghostbear@gmail.com>
2025-09-19 20:44:33 -0700

    add character metadata

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,11 +3,14 @@ extends Control
 @onready var level_label: RichTextLabel = $LevelLabel
 @onready var score_label: RichTextLabel = $ScoreLabel
 @onready var coin_label: RichTextLabel = $CoinLabel
+@onready var character_label: RichTextLabel = $CharacterLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var json
+	for i in 2:
+		json = JSON.parse_string(Files.get_file("characters/" + GlobalVariables.character + "/meta.json"))
+	character_label.text = json["name"]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
